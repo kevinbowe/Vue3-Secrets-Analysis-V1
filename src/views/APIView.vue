3d1494a8fc8c9fd6efc75e4a387bf484c553941c
kevinbowe <kevinbowe1957@gmail.com>
2024-04-15 18:20:26 -0400

    Initial commit new file:   .eslintrc.cjs new file:   .gitignore new file:   .prettierrc.json new file:   .vscode/extensions.json
    new file:   README.md
    new file:   env.d.ts
    new file:   index.html
    new file:   package-lock.json
    new file:   package.json
    new file:   tsconfig.json
    new file:   vite.config.ts
    new file:   tsconfig.app.json
    new file:   tsconfig.node.json
    new file:   zshDefineTypes_v2.sh
    
    new file:   public/favicon.ico
    
    new file:   src/App.vue
    new file:   src/main.ts
    
    new file:   src/assets/base.css
    new file:   src/assets/logo.svg
    new file:   src/assets/main.css
    
    new file:   src/components/MyConsoleUtil.d.ts
    new file:   src/components/MyConsoleUtil.ts
    new file:   src/components/MyUtil.d.ts
    new file:   src/components/MyUtil.ts
    
    new file:   src/composables/MyCompUtil.d.ts
    new file:   src/composables/MyCompUtil.ts
    
    new file:   src/router/index.d.ts
    new file:   src/router/index.ts
    
    new file:   src/stores/users.d.ts
    new file:   src/stores/users.ts
    
    new file:   src/views/APIView.vue
    new file:   src/views/DevView.vue
    new file:   src/views/HomeView.vue

---
+++ src/views/APIView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, inject, getCurrentInstance } from "vue"
+import { useUserPiniaStore } from "../stores/users"
+import '@mdi/font/css/materialdesignicons.css'
+import { fail } from "../components/MyConsoleUtil"
+/* -------------------------------------------------------------------------- */
+const piniaStore = useUserPiniaStore()		// Leave this for now
+const cb = ref()
+const cbLbl = ref("Unchecked")
+const btnTxt = ref("Hello-Start")
+const SelCb = () => {
+	cbLbl.value = cb.value ? "Unchecked" : "Checked"
+	cb.value = !cb.value
+}
+const SelBtn = () => {
+	btnTxt.value = btnTxt.value === "Hello" ? "Goodbye" : "Hello"
+}
+/* %%%%%%  On Page Load %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+
+/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+</script>
+
+<template>
+		<div>
+			<h5 class="d-flex justify-center" >( APIView )</h5>
+			<v-checkbox class="d-flex justify-center" :label="cbLbl" @click="SelCb" />
+			<div class="d-flex justify-center">
+				<v-btn :text="btnTxt" @click="SelBtn" />
+			</div>
+		</div>
+</template>
